fdc64ad4c7168dba052d6ee2cbdaead8d3ecb5d9
Ali Sina <alisina47@gmail.com>
2019-03-29 23:46:20 +0800

    Add comments explaining purpose

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
-  // Add smooth scrolling to the arrow up
-  $("a.fa-arrow").on('click', function(event) {
+// Smooth-scrolling script for browsers that do not support the
+// CSS ``scroll-behavior`` property. https://caniuse.com/#feat=css-scroll-behavior
 
-      // Prevent default anchor click behavior
-      event.preventDefault();
+$(document).ready(function() {
+  $("a.fa-arrow").on("click", function(event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+    // Store hash
+    var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      $('html, body').animate({
+    // Using jQuery's animate() method to add smooth page scroll
+    $("html, body").animate(
+      {
         scrollTop: hash
-      }, 800, function(){
-
+      },
+      800,
+      function() {
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
-      });
+      }
+    );
   });
 });
